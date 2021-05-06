4dd9ab980e085d62aef06c38d4829a1d6e91a490
DATBOI <alexsimons9999@gmail.com>
2021-05-06 18:19:25 -0500

    Pro Social Distancing

--- decorator.js
+++ decorator.js
@@ -1,1 +1,29 @@
 document.querySelectorAll('.social-count').forEach(node => node.remove())
+
+
+// document.querySelectorAll('.octicon-star').forEach(node => node.parentNode.remove())
+
+becomeHermit();
+
+function becomeHermit() {
+  document.querySelectorAll('a').forEach(node => {
+    if (node.href.indexOf('stargazers') > -1) {
+      node.remove();
+    }
+  });
+}
+
+function proSocialDistance(count) {
+  becomeHermit();
+  if(count < 10) {
+    setTimeout(()=>{
+      proSocialDistance(count + 1)
+    }, 50)
+  }  
+}
+
+document.querySelectorAll('summary[role="button"]').forEach(node => {
+  node.addEventListener('mouseover', () => {
+    proSocialDistance(0)
+  })
+});
